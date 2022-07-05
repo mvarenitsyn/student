79884412387769e0dbfa46bcf147a4508cd55736
Mikhail V <m.varenitsyn@gmail.com>
2022-07-05 17:53:40 -0400

    idea deleted

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3003
 
 app.get('/', (req:Request, res: Response) => {
-    res.send('Hello world!!!')
+    res.send('Hello world!!?!')
 })
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
